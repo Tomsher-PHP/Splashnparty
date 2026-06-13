132d50e84fefd1b46cf46f648bd502dbf17ee0c2
Jisha <jisha.tomsher@gmail.com>
2026-06-13 19:46:28 +0400

    max file size

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,5 +36,49 @@ class AppServiceProvider extends ServiceProvider
 
             $view->with('generalSettings', new Fluent($settings));
         });
+
+        // Dynamically cap file upload validation max rules to 500 KB globally
+        \Illuminate\Support\Facades\Validator::resolver(function($translator, $data, $rules, $messages, $customAttributes) {
+            foreach ($rules as $attribute => &$attributeRules) {
+                $hasFile = false;
+                $value = data_get($data, $attribute);
+                if ($value instanceof \Illuminate\Http\UploadedFile) {
+                    $hasFile = true;
+                } elseif (is_array($value)) {
+                    foreach (\Illuminate\Support\Arr::flatten($value) as $item) {
+                        if ($item instanceof \Illuminate\Http\UploadedFile) {
+                            $hasFile = true;
+                            break;
+                        }
+                    }
+                }
+
+                if ($hasFile) {
+                    if (is_array($attributeRules)) {
+                        foreach ($attributeRules as &$rule) {
+                            if (is_string($rule) && str_starts_with($rule, 'max:')) {
+                                $maxVal = (int) substr($rule, 4);
+                                if ($maxVal > 500) {
+                                    $rule = 'max:500';
+                                }
+                            }
+                        }
+                    } elseif (is_string($attributeRules)) {
+                        $parts = explode('|', $attributeRules);
+                        foreach ($parts as &$part) {
+                            if (str_starts_with($part, 'max:')) {
+                                $maxVal = (int) substr($part, 4);
+                                if ($maxVal > 500) {
+                                    $part = 'max:500';
+                                }
+                            }
+                        }
+                        $attributeRules = implode('|', $parts);
+                    }
+                }
+            }
+
+            return new \Illuminate\Validation\Validator($translator, $data, $rules, $messages, $customAttributes);
+        });
     }
 }
