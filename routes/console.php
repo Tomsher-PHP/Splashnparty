ab9f6ff6c86c721c0449b90fa58f3f94be0c3c84
Jisha <jisha.tomsher@gmail.com>
2026-06-20 09:56:27 +0400

    cron

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command('queue:work --stop-when-empty')
+        ->everyMinute()
+        ->withoutOverlapping();
