07337ad6257e79826f1aa215efd1c38c28198e90
Jisha <jisha.tomsher@gmail.com>
2026-09-08 18:29:09 +0400

    changes

--- routes/console.php
+++ routes/console.php
@@ -13,17 +13,15 @@ Schedule::command('queue:work --stop-when-empty')
         ->everyMinute()
         ->withoutOverlapping();
 
-// // Daily Branchwise Booking Report at 9:00 AM
 // Schedule::command('reports:send-daily-branch-bookings')
-//         ->dailyAt('09:00');
+//         ->everyMinute()->withoutOverlapping();
 
-// // Monthly Branchwise Booking Report on the 1st day of every month at 9:00 AM
 // Schedule::command('reports:send-monthly-branch-bookings')
-//         ->monthlyOn(1, '09:00');
+//         ->everyMinute()->withoutOverlapping();
 
 
 Schedule::command('reports:send-daily-branch-bookings')
-        ->everyMinute()->withoutOverlapping();
+        ->dailyAt('09:00');
 
 Schedule::command('reports:send-monthly-branch-bookings')
-        ->everyMinute()->withoutOverlapping();
+        ->monthlyOn(1, '09:00');
